630d5bf072982344bb647d77220285f135faea47
Nikita Oksak <stupidmaadworld@gmail.com>
2020-08-06 22:02:27 +0300

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,11 +3,12 @@ import { Card, CardImg, CardText, CardBody,
   CardTitle, Breadcrumb, BreadcrumbItem, ModalBody, Modal, ModalHeader, Label, Button, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len); 
 const minLength = (len) => (val) => val && (val.length >= len);
 
-const RenderDish = ({dish}) => {
+const RenderDish = ({ dish }) => {
     return (
       <Card>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
@@ -132,31 +133,51 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-  if(props.dish != null) 
+  if (props.isLoading) {
     return (
       <div className="container">
         <div className="row">
-            <Breadcrumb>
-                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="col-12">
-                <h3>{props.dish.name}</h3>
-                <hr />
-            </div>                
+          <div className="mx-auto mt-3">
+            <Loading />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  }
+  else if (props.dish != null)
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
         </div>
         <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              <RenderDish dish={props.dish} />
-            </div>
-            <div className="col-12 col-md-5 m-1">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
             <RenderComments comments={props.comments}
               addComment={props.addComment}
               dishId={props.dish.id}
             />
-            </div>
+          </div>
         </div>
       </div>
-  )
+    )
 }
 export default DishDetail
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle} from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
-const RenderCard = ({item}) => {
-    return(
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
+
+function RenderCard({item, isLoading, errMess}) {
+    
+    if (isLoading) {
+        return(
+            <div className="container">
+                <div className="col-12 mx-auto mt-3">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (errMess) {
+        return(
+            <h4>{errMess}</h4>
+        );
+    }
+    else 
+        return(
+            <Card>
+                <CardImg src={item.image} alt={item.name} />
+                <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
                 <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
+                </CardBody>
+            </Card>
+        );
+
 }
+
 
 const Home = (props) => {
     return(
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} />
-                </div>
+                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />                </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.promotion} />
                 </div>
